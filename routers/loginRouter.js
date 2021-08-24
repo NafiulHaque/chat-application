e2b9aab4cbfb2890fdaa87a9ecf956ada9f25862
Nafiul <nafiulhaque22@gmail.com>
2021-08-25 05:47:23 +0600

    login with jwt token setup

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -4,13 +4,29 @@ const express = require("express");
 
 
 //internal imports
-const { getLogin } = require("../controller/loginController");
+const { getLogin, login } = require("../controller/loginController");
 const decorateHtmlResponse = require("../middleware/common/decorateHtmlResponse")
+const {
+    doLoginValidationHandler,
+    doLoginValidators,
+} = require("../middleware/login/loginValidator");
 
 const router = express.Router();
 
+//set page title
+const page_title = "Login";
+
 
 //login page
-router.get("/", decorateHtmlResponse("Login"), getLogin);
+router.get("/", decorateHtmlResponse(page_title), getLogin);
+
+//process login
+router.post(
+    "/",
+    decorateHtmlResponse(page_title),
+    doLoginValidators,
+    doLoginValidationHandler,
+    login
+);
 
 module.exports = router;
