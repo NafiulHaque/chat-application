a761ba1a6d0cce1ab32ef117bd747d43431041b2
Nafiul <nafiulhaque22@gmail.com>
2021-08-27 01:34:20 +0600

    inbox page designing

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 
 //external imports
 const express = require("express");
+const http = require("http");
 const dotenv = require("dotenv");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
+const moment = require("moment");
+
+//internal imports
 const loginRouter = require("./routers/loginRouter");
 const inboxRouter = require("./routers/inboxRouter");
 const usersRouter = require("./routers/usersRouter");
@@ -13,7 +17,15 @@ const usersRouter = require("./routers/usersRouter");
 const { notFoundHandler, errorHandler } = require("./middleware/common/errorHandler");
 
 const app = express();
+const server = http.createServer(app);
 dotenv.config();
+
+// socket creation
+const io = require("socket.io")(server);
+global.io = io;
+
+// set comment as app locals
+app.locals.moment = moment;
 
 
 //database connection
@@ -34,7 +46,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-//set static folde
+//set static folder
 
 app.use(express.static(path.join(__dirname, "public")));
 
--- routers/inboxRouter.js
+++ routers/inboxRouter.js
@@ -4,15 +4,34 @@ const express = require("express");
 
 
 //internal imports
-const { getInbox } = require("../controller/inboxController");
+const {
+    getInbox,
+    searchUser,
+    addConversation,
+    getMessages,
+    sendMessage,
+} = require("../controller/inboxController");
 const { checkLogin } = require("../middleware/common/checkLogin");
-const decorateHtmlResponse = require("../middleware/common/decorateHtmlResponse")
-
+const decorateHtmlResponse = require("../middleware/common/decorateHtmlResponse");
+const attachmentUpload = require("../middleware/inbox/attachmentUpload");
 
 const router = express.Router();
 
 
 //Inbox page
 router.get("/", decorateHtmlResponse("Inbox"), checkLogin, getInbox);
+
+// search user for conversation
+router.post("/search", checkLogin, searchUser);
+
+// add conversation
+router.post("/conversation", checkLogin, addConversation);
+
+// get messages of a conversation
+router.get("/messages/:conversation_id", checkLogin, getMessages);
+
+// send message
+router.post("/message", checkLogin, attachmentUpload, sendMessage);
+
 
 module.exports = router;
